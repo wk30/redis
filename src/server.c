wk30/redis

commit ced1fa7e4ce5ed5639af5e2db35284289682e048

Ensure slots are rechecked on EXEC.
Fixes #2515.

src/server.c
@@ -2385,7 +2385,8 @@ int processCommand(client *c) {
         !(c->flags & CLIENT_MASTER) &&
         !(c->flags & CLIENT_LUA &&
           server.lua_caller->flags & CLIENT_MASTER) &&
-        !(c->cmd->getkeys_proc == NULL && c->cmd->firstkey == 0))
+        !(c->cmd->getkeys_proc == NULL && c->cmd->firstkey == 0 &&
+          c->cmd->proc != execCommand))
     {
         int hashslot;
 
@@ -2397,7 +2398,11 @@ int processCommand(client *c) {
             int error_code;
             clusterNode *n = getNodeByQuery(c,c->cmd,c->argv,c->argc,&hashslot,&error_code);
             if (n == NULL || n != server.cluster->myself) {
-                flagTransaction(c);
+                if (c->cmd->proc == execCommand) {
+                    discardTransaction(c);
+                } else {
+                    flagTransaction(c);
+                }
                 clusterRedirectClient(c,n,hashslot,error_code);
                 return C_OK;
             }

commit c594673d93d3c587cc34f4c9ece53ddc6f68b86d

Cluster: make getNodeByQuery() responsible of -CLUSTERDOWN errors.
This fixes a bug introduced by ced1fa7, and makes the code consistent
with the logic of always allowing, while the cluster is down, commands
that don't target any key.

As a side effect the code is also simpler now.

src/server.c
@@ -2391,23 +2391,17 @@ int processCommand(client *c) {
           c->cmd->proc != execCommand))
     {
         int hashslot;
-
-        if (server.cluster->state != CLUSTER_OK) {
-            flagTransaction(c);
-            clusterRedirectClient(c,NULL,0,CLUSTER_REDIR_DOWN_STATE);
-            return C_OK;
-        } else {
-            int error_code;
-            clusterNode *n = getNodeByQuery(c,c->cmd,c->argv,c->argc,&hashslot,&error_code);
-            if (n == NULL || n != server.cluster->myself) {
-                if (c->cmd->proc == execCommand) {
-                    discardTransaction(c);
-                } else {
-                    flagTransaction(c);
-                }
-                clusterRedirectClient(c,n,hashslot,error_code);
-                return C_OK;
+        int error_code;
+        clusterNode *n = getNodeByQuery(c,c->cmd,c->argv,c->argc,
+                                        &hashslot,&error_code);
+        if (n == NULL || n != server.cluster->myself) {
+            if (c->cmd->proc == execCommand) {
+                discardTransaction(c);
+            } else {
+                flagTransaction(c);
             }
+            clusterRedirectClient(c,n,hashslot,error_code);
+            return C_OK;
         }
     }
 